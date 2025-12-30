c36b75b53a9986ae70e09e1d52949420fd78d233
Cercibus <gabrielcmbrasileiro@gmail.com>
2025-12-30 16:34:18 -0300

    CH2 done, clock is set, framerate is capped

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-pygame.init()
-
 def main():
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	while True:
 		for event in pygame.event.get():
 			 if event.type == pygame.QUIT:
@@ -16,6 +17,8 @@ def main():
 		log_state()
 		screen.fill("black")
 		pygame.display.flip()
+		clock.tick(60)
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
