7179d462188a5ccd1b0e92cadd1ae1a952968e2b
Cercibus <gabrielcmbrasileiro@gmail.com>
2025-12-30 16:16:41 -0300

    Issue with screen size logging fixed

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
 			 if event.type == pygame.QUIT:
