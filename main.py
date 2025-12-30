3a0cd1aa4dab1c8323fb8d2ed3ab13831c218561
Cercibus <gabrielcmbrasileiro@gmail.com>
2025-12-30 17:43:07 -0300

    Movement all set up.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+
 
 def main():
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,12 +12,20 @@ def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 0
+	updateables = pygame.sprite.Group()
+	drawables = pygame.sprite.Group()
+	Player.containers = (updateables, drawables)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 	while True:
 		for event in pygame.event.get():
 			 if event.type == pygame.QUIT:
        				 return
 		log_state()
 		screen.fill("black")
+		for updateable in updateables:
+			updateable.update(dt)
+		for drawable in drawables:
+			drawable.draw(screen)
 		pygame.display.flip()
 		clock.tick(60)
 		dt = clock.tick(60) / 1000
